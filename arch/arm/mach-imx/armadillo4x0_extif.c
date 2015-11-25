e80a53f55e92647fe9019a04ce581a7d18c8318c
Makoto Harada <makoto.harada@atmark-techno.com>
2015-11-25 22:36:16 +0900

    ARM: imx: armadillo4x0: add support for SSI1 on CON11(AUD5)
    There are a few restrictions:
    1. Only support CON11(AUD5), no support for CON9(AUD6)
    2. Support SSI1 only, no support for SSI2

--- arch/arm/mach-imx/armadillo4x0_extif.c
+++ arch/arm/mach-imx/armadillo4x0_extif.c
@@ -87,6 +87,10 @@ static unsigned long  __maybe_unused pin_cfgs_100kup_hys[] = {
 	PAD_CTL_PUS_100K_UP | PAD_CTL_HYS,
 };
 
+static unsigned long  __maybe_unused pin_cfgs_100kup_sre_fast[] = {
+	PAD_CTL_PUS_100K_UP | PAD_CTL_SRE_FAST,
+};
+
 static unsigned long __maybe_unused pin_cfgs_sre_fast[] = {
 	PAD_CTL_SRE_FAST,
 };
@@ -592,6 +596,34 @@ static const struct pinctrl_map armadillo4x0_con11_pinctrl_map[] = {
 	PIN_MAP_CONFIGS_PIN_DEFAULT("imx21-fb.0", "imx25-pinctrl.0",
 				    "MX25_PAD_GRP_DSE_LCD", pin_cfgs_dse_low),
 
+	/* aud5 */
+#if defined(CONFIG_ARMADILLO4X0_AUD5_CON11)
+	PIN_MAP_MUX_GROUP_DEFAULT("imx31-audmux.0", "imx25-pinctrl.0",
+				  "kpp_col0__aud5_txd", "aud5"),
+	PIN_MAP_MUX_GROUP_DEFAULT("imx31-audmux.0", "imx25-pinctrl.0",
+				  "kpp_col1__aud5_rxd", "aud5"),
+	PIN_MAP_MUX_GROUP_DEFAULT("imx31-audmux.0", "imx25-pinctrl.0",
+				  "kpp_col2__aud5_txc", "aud5"),
+	PIN_MAP_MUX_GROUP_DEFAULT("imx31-audmux.0", "imx25-pinctrl.0",
+				  "kpp_col3__aud5_txfs", "aud5"),
+	PIN_MAP_MUX_GROUP_DEFAULT("imx31-audmux.0", "imx25-pinctrl.0",
+				  "kpp_row2__aud5_rxc", "aud5"),
+	PIN_MAP_MUX_GROUP_DEFAULT("imx31-audmux.0", "imx25-pinctrl.0",
+				  "kpp_row3__aud5_rxfs", "aud5"),
+	PIN_MAP_CONFIGS_PIN_DEFAULT("imx31-audmux.0", "imx25-pinctrl.0",
+				    "MX25_PAD_KPP_COL0", pin_cfgs_100kup),
+	PIN_MAP_CONFIGS_PIN_DEFAULT("imx31-audmux.0", "imx25-pinctrl.0",
+				    "MX25_PAD_KPP_COL1", pin_cfgs_100kup),
+	PIN_MAP_CONFIGS_PIN_DEFAULT("imx31-audmux.0", "imx25-pinctrl.0",
+				    "MX25_PAD_KPP_COL2", pin_cfgs_100kup),
+	PIN_MAP_CONFIGS_PIN_DEFAULT("imx31-audmux.0", "imx25-pinctrl.0",
+				    "MX25_PAD_KPP_COL3", pin_cfgs_100kup),
+	PIN_MAP_CONFIGS_PIN_DEFAULT("imx31-audmux.0", "imx25-pinctrl.0",
+				    "MX25_PAD_KPP_ROW2", pin_cfgs_100kup_sre_fast),
+	PIN_MAP_CONFIGS_PIN_DEFAULT("imx31-audmux.0", "imx25-pinctrl.0",
+				    "MX25_PAD_KPP_ROW3", pin_cfgs_100kup),
+#endif
+
 	/* uart3 */
 #if defined(CONFIG_ARMADILLO4X0_UART3_CON11)
 	PIN_MAP_MUX_GROUP_DEFAULT("imx21-uart.2", "imx25-pinctrl.0",
@@ -757,6 +789,11 @@ static struct i2c_board_info armadillo4x0_i2c2[] = {
 	{
 		I2C_BOARD_INFO("s35390a", 0x30),
 		/* irq number is run-time assigned */
+	},
+#endif
+#if defined(CONFIG_ARMADILLO4X0_I2C3_CON11_WM8978)
+	{
+		I2C_BOARD_INFO("wm8978", 0x1a),
 	},
 #endif
 };
@@ -1115,8 +1152,16 @@ static void __init armadillo4x0_touchscreen_init(void)
 				NULL, 0, NULL, 0);
 }
 
+static struct imx_ssi_platform_data armadillo4x0_ssi0_pdata = {
+	.flags = IMX_SSI_SYN | IMX_SSI_NET,
+};
+
 static void __init armadillo4x0_audio_init(void)
 {
+	if (IS_ENABLED(CONFIG_SSI_MXC_SELECT1)) {
+		imx25_add_imx_ssi(0, &armadillo4x0_ssi0_pdata);
+		imx_add_platform_device("armadillo4x0_wm8978", 0, NULL, 0, NULL, 0);
+	}
 }
 #endif
 
